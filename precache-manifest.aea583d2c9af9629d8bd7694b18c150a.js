95dc40545853fa434df24e7a4cfe4fdca819ce64
Bijay Subedi <66840690+subedibj@users.noreply.github.com>
2020-08-15 22:10:06 -0700

    Updates

---
+++ precache-manifest.aea583d2c9af9629d8bd7694b18c150a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cac755bd064c556df1716c6221c2b86",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "7c5f520d8c280b58914a",
+    "url": "/project1/static/css/main.5566fac6.chunk.css"
+  },
+  {
+    "revision": "063a3897fe664475aefd",
+    "url": "/project1/static/js/2.ae988e72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project1/static/js/2.ae988e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5f520d8c280b58914a",
+    "url": "/project1/static/js/main.ea6e277b.chunk.js"
+  },
+  {
+    "revision": "ace76330201db7d096c4",
+    "url": "/project1/static/js/runtime-main.33131a4a.js"
+  },
+  {
+    "revision": "5cea79da66b347f17f160b448f33560d",
+    "url": "/project1/static/media/e-commerce.5cea79da.jpg"
+  },
+  {
+    "revision": "4fbe4b587eafcbfae1c8f715c41265f1",
+    "url": "/project1/static/media/img0.4fbe4b58.jpg"
+  },
+  {
+    "revision": "60f387e0c9cc8dd7cd006be80f7403cf",
+    "url": "/project1/static/media/img1.60f387e0.jpg"
+  },
+  {
+    "revision": "8281b18c5f554f6b72269a6b2ea792ed",
+    "url": "/project1/static/media/img2.8281b18c.jpg"
+  },
+  {
+    "revision": "3919818d13454d8f8d3a5628a6a2308e",
+    "url": "/project1/static/media/img5.3919818d.jpg"
+  },
+  {
+    "revision": "50250572b70b831af6efd8bd2ea5ba71",
+    "url": "/project1/static/media/men1.50250572.jpg"
+  },
+  {
+    "revision": "9a8e04247a8490b19f97799912dc6d58",
+    "url": "/project1/static/media/men2.9a8e0424.jpg"
+  },
+  {
+    "revision": "acf21f32ff6ab1f45976cf412fff4475",
+    "url": "/project1/static/media/men4.acf21f32.jpg"
+  },
+  {
+    "revision": "400c7815c062d897ca3e1f425caa504f",
+    "url": "/project1/static/media/organization.400c7815.png"
+  },
+  {
+    "revision": "6b3824fd130d2c3ecc862866877f6141",
+    "url": "/project1/static/media/schoolsoftware.6b3824fd.jpg"
+  },
+  {
+    "revision": "2d44032aff1af7ee81fdcd528705727a",
+    "url": "/project1/static/media/woman2.2d44032a.jpg"
+  }
+]);
